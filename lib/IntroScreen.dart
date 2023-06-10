07025cc25ac8de026be5148649ae9f1dc3cd3959
NalluSwami <aringopalan13489@gmail.com>
2023-06-11 04:00:13 +0530

    Quest section Complete

--- lib/IntroScreen.dart
+++ lib/IntroScreen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:study_ruin/BottomNavigator.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ToDoPage extends StatefulWidget {
   final String name;
   final String password;
+  
 
   const ToDoPage({Key? key, required this.name, required this.password}) : super(key: key);
 
@@ -12,8 +14,12 @@ class ToDoPage extends StatefulWidget {
 }
 
 class _ToDoPageState extends State<ToDoPage> {
-  List<String> tasks = [];
+  int health = 100;
+  int experience = 0;
+  int struggle =1;
 
+  List<String> tasks = [];
+  
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -73,7 +79,7 @@ class _ToDoPageState extends State<ToDoPage> {
                             ),
                             SizedBox(width: 8),
                             Text(
-                              'Health: 100',
+                              'Health:'+health.toString()+'/100',
                               style: TextStyle(
                                 color: const Color.fromARGB(255, 255, 255, 255),
                                 fontSize: 18,
@@ -90,7 +96,7 @@ class _ToDoPageState extends State<ToDoPage> {
                             ),
                             SizedBox(width: 8),
                             Text(
-                              'Experience: 0/100',
+                              'Experience:'+experience.toString()+'/100',
                               style: TextStyle(
                                 color: const Color.fromARGB(255, 255, 255, 255),
                                 fontSize: 18,
@@ -107,7 +113,7 @@ class _ToDoPageState extends State<ToDoPage> {
                             ),
                             SizedBox(width: 8),
                             Text(
-                              'Struggle: 1/NeverEnds',
+                              'Struggle:'+struggle.toString(),
                               style: TextStyle(
                                 color: const Color.fromARGB(255, 255, 255, 255),
                                 fontSize: 18,
@@ -133,44 +139,67 @@ class _ToDoPageState extends State<ToDoPage> {
               ),
               SizedBox(height: 8),
               Expanded(
-                child: ListView.separated(
-  itemCount: tasks.length,
-  separatorBuilder: (context, index) => Divider(),
-  itemBuilder: (context, index) {
-    final task = tasks[index];
-    bool isTaskCompleted = false;
+  child: ListView.separated(
+    itemCount: tasks.length,
+    separatorBuilder: (context, index) => Divider(),
+    itemBuilder: (context, index) {
+  final task = tasks[index];
+  bool isTaskCompleted = false;
 
-    return Container(
-      color: Color.fromARGB(255, 16, 50, 161), // Customize the tile background color as needed
-      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      child: ListTile(
-        contentPadding: EdgeInsets.zero,
-        leading: CircleAvatar(
-          backgroundImage: NetworkImage('https://example.com/task_image.png'), // Replace with the URL of the task's image
-        ),
-        title: Text(
+  return Container(
+    decoration: BoxDecoration(
+      color: Color.fromARGB(255, 16, 50, 161),
+      borderRadius: BorderRadius.circular(10), // Rounded corners
+    ),
+    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+    child: ListTile(
+  contentPadding: EdgeInsets.zero,
+  leading: CircleAvatar(
+    backgroundColor: Colors.transparent,
+  ),
+  title: Center(
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
           task,
           style: TextStyle(
             fontSize: 16,
             fontWeight: FontWeight.bold,
+            color: Colors.white,
           ),
         ),
-        trailing: Checkbox(
-          value: isTaskCompleted,
-          onChanged: (value) {
-            setState(() {
-              isTaskCompleted = value!;
-              if (isTaskCompleted) {
-                tasks.removeAt(index);
-              }
-            });
-          },
-        ),
-      ),
-    );
-  },
+        SizedBox(height: 8),
+      ],
+    ),
+  ),
+  trailing: Checkbox(
+    value: isTaskCompleted,
+    onChanged: (value) {
+      setState(() {
+        isTaskCompleted = value!;
+        if (isTaskCompleted) {
+          tasks.removeAt(index);
+          experience += 5; // Increase experience by 10
+        }
+      });
+    },
+  ),
+  tileColor: Color.fromARGB(255, 16, 50, 161), // Background color
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(10), // Rounded corners
+    side: BorderSide(color: Colors.white), // Border color
+  ),
 ),
+
+  );
+},
+
+  ),
 ),
+
+
+
             ],
           ),
         ),
@@ -179,122 +208,124 @@ class _ToDoPageState extends State<ToDoPage> {
   }
 
   void _addTask() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        String newTask = '';
-        String selectedTaskType = '';
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      String newTask = '';
+      String selectedTaskType = '';
 
-        return AlertDialog(
-          title: Text(
-            'Add Task',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
+      return AlertDialog(
+        title: Text(
+          'Add Task',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
           ),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextField(
-                onChanged: (value) {
-                  newTask = value;
-                },
-                decoration: InputDecoration(
-                  hintText: 'Enter task...',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Task Type:',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 8),
-              Wrap(
-                spacing: 8,
-                runSpacing: 8,
-                children: [
-                  ChoiceChip(
-                    label: Text('Maths'),
-                    selected: selectedTaskType == 'Maths',
-                    onSelected: (selected) {
-                      setState(() {
-                        if (selected) {
-                          selectedTaskType = 'Maths';
-                        }
-                      });
-                    },
-                  ),
-                  ChoiceChip(
-                    label: Text('Physics'),
-                    selected: selectedTaskType == 'Physics',
-                    onSelected: (selected) {
-                      setState(() {
-                        if (selected) {
-                          selectedTaskType = 'Physics';
-                        }
-                      });
-                    },
-                  ),
-                  ChoiceChip(
-                    label: Text('Chemistry'),
-                    selected: selectedTaskType == 'Chemistry',
-                    onSelected: (selected) {
-                      setState(() {
-                        if (selected) {
-                          selectedTaskType = 'Chemistry';
-                        }
-                      });
-                    },
-                  ),
-                  ChoiceChip(
-                    label: Text('Computer-Science'),
-                    selected: selectedTaskType == 'Computer-Science',
-                    onSelected: (selected) {
-                      setState(() {
-                        if (selected) {
-                          selectedTaskType = 'Computer-Science';
-                        }
-                      });
-                    },
-                  ),
-                  ChoiceChip(
-                    label: Text('Literature'),
-                    selected: selectedTaskType == 'Literature',
-                    onSelected: (selected) {
-                      setState(() {
-                        if (selected) {
-                          selectedTaskType = 'Literature';
-                        }
-                      });
-                    },
-                  ),
-                  // Add more options as needed
-                ],
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  tasks.add('$newTask ($selectedTaskType)');
-                });
-                Navigator.of(context).pop();
+        ),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              onChanged: (value) {
+                newTask = value;
               },
-              child: Text('Add'),
+              decoration: InputDecoration(
+                hintText: 'Enter task...',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Task Type:',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Wrap(
+              spacing: 8,
+              runSpacing: 8,
+              children: [
+                ChoiceChip(
+                  label: Text('Maths'),
+                  selected: selectedTaskType == 'Maths',
+                  onSelected: (selected) {
+                    setState(() {
+                      if (selected) {
+                        selectedTaskType = 'Maths';
+                      }
+                    });
+                  },
+                ),
+                ChoiceChip(
+                  label: Text('Physics'),
+                  selected: selectedTaskType == 'Physics',
+                  onSelected: (selected) {
+                    setState(() {
+                      if (selected) {
+                        selectedTaskType = 'Physics';
+                      }
+                    });
+                  },
+                ),
+                ChoiceChip(
+                  label: Text('Chemistry'),
+                  selected: selectedTaskType == 'Chemistry',
+                  onSelected: (selected) {
+                    setState(() {
+                      if (selected) {
+                        selectedTaskType = 'Chemistry';
+                      }
+                    });
+                  },
+                ),
+                ChoiceChip(
+                  label: Text('Computer-Science'),
+                  selected: selectedTaskType == 'Computer-Science',
+                  onSelected: (selected) {
+                    setState(() {
+                      if (selected) {
+                        selectedTaskType = 'Computer-Science';
+                      }
+                    });
+                  },
+                ),
+                ChoiceChip(
+                  label: Text('Literature'),
+                  selected: selectedTaskType == 'Literature',
+                  onSelected: (selected) {
+                    setState(() {
+                      if (selected) {
+                        selectedTaskType = 'Literature';
+                      }
+                    });
+                  },
+                ),
+                // Add more options as needed
+              ],
             ),
           ],
-        );
-      },
-    );
-  }
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              setState(() {
+                tasks.add('$newTask $selectedTaskType');
+                 // Increase experience by 10
+              });
+              Navigator.of(context).pop();
+            },
+            child: Text('Add'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
 
   Future<void> _launchURL(String url) async {
     if (await canLaunch(url)) {
@@ -304,3 +335,5 @@ class _ToDoPageState extends State<ToDoPage> {
     }
   }
 }
+
+
