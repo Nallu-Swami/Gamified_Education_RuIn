c6a71b8f3112e4d90a50a70e6e74610fba99e0b0
NalluSwami <aringopalan13489@gmail.com>
2023-06-10 22:19:48 +0530

    Added Slider Components

--- lib/Intro_Slider.dart
+++ lib/Intro_Slider.dart
@@ -2,20 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Study_RuIns',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      title: 'Intro Slider',
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: IntroSlider(),
     );
   }
@@ -34,26 +29,48 @@ class _IntroSliderState extends State<IntroSlider> {
   ];
 
   int currentIndex = 0;
+  CarouselController carouselController = CarouselController();
+
+  @override
+  void initState() {
+    super.initState();
+    startAutoSlide();
+  }
+
+  @override
+  void dispose() {
+    stopAutoSlide();
+    super.dispose();
+  }
+
+  void startAutoSlide() {
+    Future.delayed(Duration(seconds: 3), () {
+      carouselController.nextPage();
+      startAutoSlide();
+    });
+  }
+
+  void stopAutoSlide() {
+    carouselController.stopAutoPlay();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Study RuIns'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
+      body: Stack(
+        children: [
           CarouselSlider(
+            carouselController: carouselController,
             options: CarouselOptions(
-              height: MediaQuery.of(context).size.height - AppBar().preferredSize.height - 56,
+              height: double.infinity,
+              viewportFraction: 1.0,
               enableInfiniteScroll: false,
               onPageChanged: (index, reason) {
                 setState(() {
                   currentIndex = index;
                 });
               },
+              autoPlay: false,
             ),
             items: imageList.map((imagePath) {
               return Builder(
@@ -66,21 +83,18 @@ class _IntroSliderState extends State<IntroSlider> {
               );
             }).toList(),
           ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              // Handle button action
-              if (currentIndex == imageList.length - 1) {
-                // Close the intro slider if it's on the last image
-                Navigator.pop(context);
-              } else {
-                // Go to the next image
-                setState(() {
-                  currentIndex++;
-                });
-              }
-            },
-            child: Text('Next'),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ElevatedButton(
+              onPressed: () {
+                if (currentIndex == imageList.length - 1) {
+                  Navigator.pop(context);
+                } else {
+                  carouselController.nextPage();
+                }
+              },
+              child: Text('Next'),
+            ),
           ),
         ],
       ),
